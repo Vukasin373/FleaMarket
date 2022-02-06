380072f92101336f482428d445e08713ca9aadc3
Aleksandar-Elfak <77788823+Aleksandar-Elfak@users.noreply.github.com>
2022-02-06 17:48:15 +0100

    a

---
+++ Server/Services/MongoSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Server.Services
+{
+
+    public class MongoSession
+    {
+
+        var settings = MongoClientSettings.FromConnectionString("mongodb+srv://admin:admin@fleamarket.z2vvq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+        var client = new MongoClient(settings);
+        var database = client.GetDatabase("FleaMarket");
+
+    }
+}
