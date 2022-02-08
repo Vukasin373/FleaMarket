78a675df78379c75e831a6612e0970cdfb5633d1
Aleksandar-Elfak <77788823+Aleksandar-Elfak@users.noreply.github.com>
2022-02-08 17:56:28 +0100

    image src

--- Client/start.js
+++ Client/start.js
@@ -6,8 +6,75 @@ export class Start {
 		this.username = null;
 	}
 
+	// draw(host) {
+	// 	let hub = new Hub();
+	// 	hub.draw(host);
+	// }
 	draw(host) {
-		let hub = new Hub();
-		hub.draw(host);
+		this.container = document.createElement("div");
+		this.container.className = "main1";
+		host.appendChild(this.container);
+
+		const left = document.createElement("div");
+		left.className = "left1";
+		this.container.appendChild(left);
+
+		const banner = document.createElement("img");
+		banner.src = "./banner.png";
+		banner.className = "banner1";
+		left.appendChild(banner);
+
+		//login form
+		const login = document.createElement("div");
+		login.className = "login1 ui raised segment";
+		left.appendChild(login);
+
+		const loginform = document.createElement("div");
+		loginform.className = "loginForm1 ui form";
+		login.appendChild(loginform);
+
+		const loginLabel = document.createElement("label");
+		loginLabel.className = "loginLabel1 ui Large green ribbon label";
+		loginLabel.innerHTML = "Sign in:";
+		loginform.appendChild(loginLabel);
+
+		//login username
+		const loginUsername = document.createElement("div");
+		loginUsername.className = "field";
+		loginform.appendChild(loginUsername);
+
+		const loginUsernameLbl = document.createElement("label");
+		loginUsernameLbl.innerHTML = "Username";
+		loginUsername.appendChild(loginUsernameLbl);
+
+		const loginUsernameInput = document.createElement("input");
+		loginUsernameInput.type = "text";
+		loginUsernameInput.name = "username";
+		loginUsernameInput.placeholder = "Username";
+		loginUsername.appendChild(loginUsernameInput);
+
+		//login password
+		const loginPassword = document.createElement("div");
+		loginPassword.className = "field";
+		loginform.appendChild(loginPassword);
+
+		const loginPasswordLbl = document.createElement("label");
+		loginPasswordLbl.innerHTML = "Password";
+		loginPassword.appendChild(loginPasswordLbl);
+
+		const loginPasswordInput = document.createElement("input");
+		loginPasswordInput.type = "text";
+		loginPasswordInput.name = "Password";
+		loginPasswordInput.placeholder = "Username";
+		loginPassword.appendChild(loginPasswordInput);
+
+		const LoginButton = document.createElement("button");
+		LoginButton.className = "ui green button";
+		LoginButton.innerHTML = "Confirm";
+		loginform.appendChild(LoginButton);
+
+		const register = document.createElement("div");
+		register.className = "register1";
+		this.container.appendChild(register);
 	}
 }
