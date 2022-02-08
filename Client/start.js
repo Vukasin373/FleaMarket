864910f1b4c2b36e98cd532efac61d34058bc9ba
Aleksandar-Elfak <77788823+Aleksandar-Elfak@users.noreply.github.com>
2022-02-08 18:19:31 +0100

    izgled

--- Client/start.js
+++ Client/start.js
@@ -63,9 +63,8 @@ export class Start {
 		loginPassword.appendChild(loginPasswordLbl);
 
 		const loginPasswordInput = document.createElement("input");
-		loginPasswordInput.type = "text";
+		loginPasswordInput.type = "password";
 		loginPasswordInput.name = "Password";
-		loginPasswordInput.placeholder = "Username";
 		loginPassword.appendChild(loginPasswordInput);
 
 		const LoginButton = document.createElement("button");
@@ -76,5 +75,108 @@ export class Start {
 		const register = document.createElement("div");
 		register.className = "register1";
 		this.container.appendChild(register);
+
+		const registerform = document.createElement("div");
+		registerform.className = "registerForm1 ui raised segment form";
+		register.appendChild(registerform);
+
+		const registerLabel = document.createElement("label");
+		registerLabel.className = "registerLabel1 ui Large green ribbon label";
+		registerLabel.innerHTML = "Sign up:";
+		registerform.appendChild(registerLabel);
+
+		//register username
+		const registerUsername = document.createElement("div");
+		registerUsername.className = "field";
+		registerform.appendChild(registerUsername);
+
+		const registerUsernameLbl = document.createElement("label");
+		registerUsernameLbl.innerHTML = "Username";
+		registerUsername.appendChild(registerUsernameLbl);
+
+		const registerUsernameInput = document.createElement("input");
+		registerUsernameInput.type = "text";
+		registerUsernameInput.name = "username";
+		registerUsernameInput.placeholder = "Username";
+		registerUsername.appendChild(registerUsernameInput);
+
+		//register password
+		const registerPassword = document.createElement("div");
+		registerPassword.className = "field";
+		registerform.appendChild(registerPassword);
+
+		const registerPasswordLbl = document.createElement("label");
+		registerPasswordLbl.innerHTML = "Password";
+		registerPassword.appendChild(registerPasswordLbl);
+
+		const registerPasswordInput = document.createElement("input");
+		registerPasswordInput.type = "password";
+		registerPasswordInput.name = "Password";
+		registerPassword.appendChild(registerPasswordInput);
+
+		//register First name
+		const register1 = document.createElement("div");
+		register1.className = "field";
+		registerform.appendChild(register1);
+
+		const registerLbl1 = document.createElement("label");
+		registerLbl1.innerHTML = "First name";
+		register1.appendChild(registerLbl1);
+
+		const registerInput1 = document.createElement("input");
+		registerInput1.type = "text";
+		registerInput1.name = "FirstName";
+		registerInput1.placeholder = "First name";
+		register1.appendChild(registerInput1);
+
+		//register Last name
+		const register2 = document.createElement("div");
+		register2.className = "field";
+		registerform.appendChild(register2);
+
+		const registerLbl2 = document.createElement("label");
+		registerLbl2.innerHTML = "Last name";
+		register2.appendChild(registerLbl2);
+
+		const registerInput2 = document.createElement("input");
+		registerInput2.type = "text";
+		registerInput2.name = "FirstName";
+		registerInput2.placeholder = "Last name";
+		register2.appendChild(registerInput2);
+
+		//register city
+		const register3 = document.createElement("div");
+		register3.className = "field";
+		registerform.appendChild(register3);
+
+		const registerLbl3 = document.createElement("label");
+		registerLbl3.innerHTML = "City";
+		register3.appendChild(registerLbl3);
+
+		const registerInput3 = document.createElement("input");
+		registerInput3.type = "text";
+		registerInput3.name = "FirstName";
+		registerInput3.placeholder = "City";
+		register3.appendChild(registerInput3);
+
+		//register contact
+		const register4 = document.createElement("div");
+		register4.className = "field";
+		registerform.appendChild(register4);
+
+		const registerLbl4 = document.createElement("label");
+		registerLbl4.innerHTML = "Contact";
+		register4.appendChild(registerLbl4);
+
+		const registerInput4 = document.createElement("input");
+		registerInput4.type = "text";
+		registerInput4.name = "FirstName";
+		registerInput4.placeholder = "Constact";
+		register4.appendChild(registerInput4);
+
+		const RegisterButton = document.createElement("button");
+		RegisterButton.className = "ui green button";
+		RegisterButton.innerHTML = "Confirm";
+		registerform.appendChild(RegisterButton);
 	}
 }
