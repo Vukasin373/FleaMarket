d7d41f24594280496024a2dbef89ec3f8f3d9477
Aleksandar-Elfak <77788823+Aleksandar-Elfak@users.noreply.github.com>
2022-02-09 16:59:33 +0100

    id

--- Client/notificationsAndBarter.js
+++ Client/notificationsAndBarter.js
@@ -1,3 +1,5 @@
+import { Notification } from "./Entities/Notification.js";
+
 export class NotificationsAndBarter {
 	constructor(user) {
 		this.user = user;
@@ -7,5 +9,17 @@ export class NotificationsAndBarter {
 		const mainDiv1 = document.createElement("div");
 		mainDiv1.className = "mainDiv1";
 		host.appendChild(mainDiv1);
+
+		const notifList = document.createElement("div");
+		notifList.className = "notifDiv1";
+		mainDiv1.appendChild(notifList);
+
+		fetch(
+			`https://localhost:7085/FleaMarket/GetNotifications/${this.user.username}`
+		).then((p) => {
+			p.json().then((products) => {
+				console.log(products);
+			});
+		});
 	}
 }
